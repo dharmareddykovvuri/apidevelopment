a6b88caeefd25c6782b3c07c15204de36824b865
Dharma <kovvuridharmareddy014@gmail.com>
2023-06-13 22:08:21 +0530

    API Development

--- src/main/java/co/volopay/apidevelopment/ApidataController.java
+++ src/main/java/co/volopay/apidevelopment/ApidataController.java
@@ -1,13 +1,8 @@
 package co.volopay.apidevelopment;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import org.joda.time.format.*;
-import org.joda.time.*;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,32 +13,35 @@ public class ApidataController{
     
     @GetMapping("/api/total_items")
     public int getTotalItemsSoldInQ3(
-        @RequestParam("start") String startStr,
-        @RequestParam("end") String endStr,
-        @RequestParam("dept") String dept
-    ) throws IOException, ParseException {
-        
+        @RequestParam String startStr,
+        @RequestParam String endStr,
+        @RequestParam String dept
+    )  {
         String csvFilePath = "D:/Desktop/data.csv"; // Replace with the actual path to your CSV file
         
         int totalSales = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
             String line;
+            Date start= new SimpleDateFormat("yyyy-mm-dd").parse(startStr);
+            Date end= new SimpleDateFormat("yyyy-mm-dd").parse(endStr);
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(","); // Assuming CSV data is comma-separated
                 String dateString = values[1];
-                 DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZ");
-                DateTime start = formatter.parseDateTime(startStr);
-                DateTime end = formatter.parseDateTime(endStr);
-                DateTime dt=formatter.parseDateTime(dateString);
-                if(start.isBefore(dt) && dt.isBefore(end) && dept.equals(values[3])){
-                    totalSales +=Integer.parseInt(values[5]);
+                if(dateString.length()>10){
+                String dateStr = dateString.substring(0, 10);
+                Date date = new SimpleDateFormat("yyyy-mm-dd").parse(dateStr);
+                int comp1 = start.compareTo(date);
+                int comp2 =end.compareTo(date);
+                if(comp1<=0 && comp2>=0 && dept.equalsIgnoreCase(values[3])){
+                    totalSales += Integer.parseInt(values[5]);
                 }
-              
-                }
-                
-                
             }
+
+                
+                }   
+            }
+            catch(Exception e){e.printStackTrace();}
             return totalSales;
         }
         
